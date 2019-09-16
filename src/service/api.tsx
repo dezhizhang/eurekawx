7b67f10adb5ed97f281ce6f3d23c5d822e7f82c1
dezhizhang <1541609448@qq.com>
2019-09-16 22:27:17 +0800

    新增请求接口

---
+++ src/service/api.tsx
@@ -0,0 +1,8 @@
+import {stringify} from 'qs';
+import request from '../utils/request';
+import Config from '../common/config';
+
+export async function getInTheaters(params) {
+  // return request(`${Config.API_HOST}/movie/in_theaters?${stringify(params)}`);
+}
+
---
+++ src/utils/request.tsx
@@ -0,0 +1,26 @@
+import Taro from '@tarojs/taro'
+export default function request(url, options) {
+  let newOptions = { ...options };
+  if (newOptions.method === 'POST' || newOptions.method === 'PUT') {
+    if (!(newOptions.body instanceof FormData)) {
+      newOptions.header = {
+        Accept: 'application/json',
+        'Content-Type': 'application/json; charset=utf-8',
+        ...newOptions.header
+      };
+    } else {
+      // newOptions.body is FormData
+      newOptions.header = {
+        Accept: 'application/json',
+        ...newOptions.header
+      };
+    }
+  } else {
+    newOptions.header = {
+      'Content-Type': 'json',  // 这里是特殊处理
+      ...newOptions.header
+    }
+  }
+
+  return Taro.request({url, ...newOptions})
+}
