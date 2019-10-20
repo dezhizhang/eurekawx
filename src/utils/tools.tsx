9f6a01e3e425473d26f35d9f798e78d72e8b5646
dezhizhang <1541609448@qq.com>
2019-10-20 22:56:34 +0800

    修改

--- src/utils/tools.tsx
+++ src/utils/tools.tsx
@@ -1,5 +1,4 @@
-// export const baseURL = 'https://www.eureka.net.cn'
-export const baseURL = 'http://127.0.0.1:7001'
+export const baseURL = 'https://www.eureka.net.cn'
 import Taro from '@tarojs/taro'
 
 //提示
