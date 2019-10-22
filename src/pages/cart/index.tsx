9fe5b4cac5d623e26efc8359c1863d14756fdc88
dezhizhang <1541609448@qq.com>
2019-10-22 21:01:04 +0800

    修改

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -7,16 +7,6 @@ import arror from '../../images/icon/arrow.png'
 import detailSwiper from '../../images/detail_swiper.png'
 import  './index.less'
 
-// #region 书写注意
-//
-// 目前 typescript 版本还无法在装饰器模式下将 Props 注入到 Taro.Component 中的 props 属性
-// 需要显示声明 connect 的参数类型并通过 interface 的方式指定 Taro.Component 子类的 props
-// 这样才能完成类型检查和 IDE 的自动提示
-// 使用函数模式则无此限制
-// ref: https://github.com/DefinitelyTyped/DefinitelyTyped/issues/20796
-//
-// #endregion
-
 type PageStateProps = {
   counter: {
     num: number
@@ -54,14 +44,7 @@ interface Index {
 }))
 class Index extends Component {
 
-    /**
-   * 指定config的类型声明为: Taro.Config
-   *
-   * 由于 typescript 对于 object 类型推导只能推出 Key 的基本类型
-   * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
-   * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
-   */
-    config: Config = {
+  config: Config = {
     navigationBarTitleText: '购物车'
   }
 
@@ -162,7 +145,6 @@ class Index extends Component {
             </View>
           </View>
         </View>
-        
         <View className="bottom">
           <View className="bottom-wrapper">
             <View className="item-one">
@@ -174,17 +156,10 @@ class Index extends Component {
             <View className="item-six">去支付</View>
           </View>
         </View>
-        
       </ScrollView>
     )
   }
 }
 
-// #region 导出注意
-//
-// 经过上面的声明后需要将导出的 Taro.Component 子类修改为子类本身的 props 属性
-// 这样在使用这个子类时 Ts 才不会提示缺少 JSX 类型参数错误
-//
-// #endregion
 
 export default Index as ComponentClass<PageOwnProps, PageState>
