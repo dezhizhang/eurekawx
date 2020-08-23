89ae7418fde52db1465098747b4943296796bf7b
zhangdezhi <zhangdezhi@inman.cc>
2020-08-23 10:00:03 +0800

    订单列表完成

--- src/pages/my/index.tsx
+++ src/pages/my/index.tsx
@@ -212,7 +212,7 @@ class Index extends Component {
                       </View>
                       <View className="item-bottom">所有订单</View>
                     </View>
-                    <View className="bottom-item" onClick={() => this.handleToOrder(0)}>
+                    <View className="bottom-item" onClick={() => this.handleToOrder(3)}>
                       <View className="item-top">
                         <Image src={allOrder} className="image"/>
                       </View>
