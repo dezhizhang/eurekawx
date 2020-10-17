ee5ef217b87c2049d440f849faee6febf72cbc00
zhangdezhi <1541609448@qq.com>
2020-10-17 23:14:04 +0800

    修改报错

--- config/index.js
+++ config/index.js
@@ -95,7 +95,7 @@ const config = {
 }
 
 module.exports = function (merge) {
-  if (process.env.NODE_ENV === 'production') {
+  if (process.env.NODE_ENV === 'development') {
     return merge({}, config, require('./dev'))
   }
   return merge({}, config, require('./prod'))
