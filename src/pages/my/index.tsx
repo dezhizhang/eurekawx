bcba6c059288a9b7a97a10020c3823c663aa7701
dezhizhang <1541609448@qq.com>
2020-12-07 01:07:52 +0800

    tijiao

--- src/pages/my/index.tsx
+++ src/pages/my/index.tsx
@@ -223,7 +223,7 @@ export default class Index extends Component<IndexProps,IndexState> {
                  </View>
                  <View className="text-left">我的消息</View>
                  <View className="text-right">
-                   <View className="text-number">16</View>
+                   <View className="text-number">0</View>
                  </View>
                  <View className="icon-right">
                    <Image src={arrow} className="image"/>
