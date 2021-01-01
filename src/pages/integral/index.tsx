23c9606fc73ea2077912b7ddab896c2a4ce00f51
zhangdezhi <1541609448@qq.com>
2021-01-01 22:05:58 +0800

    添加积分

--- src/pages/integral/index.tsx
+++ src/pages/integral/index.tsx
@@ -20,7 +20,16 @@ export default class Index extends Component<IndexProps,IndexState> {
                 </View>
                 <View className="integral-wrapper">
                     <View className="wrapper-header">
-                        <View className="wrapper-panel">1111</View>
+                        <View className="wrapper-panel">
+                            <View className="panel-item panel-left">
+                                <View className="left-text">我的积分</View>
+                                <View className="left-number">2000</View>
+                            </View>
+                            <View className="panel-item panel-right">
+                                <View className="right-text">立即签到</View>
+                                <View className="right-text text-shop">积分商城</View>
+                            </View>
+                        </View>
                     </View>
                     <View className="wrapper-body">
                         <View className="body-sign"></View>
